f16093682566f26dd1ea6f6105501de666c6fc98
Prashant Sinha <prashant+git@noop.pw>
2016-12-24 01:17:40 +0530

    Move utility components into utils.jsx

---
+++ placet/utils.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import { Button } from '@blueprintjs/core'
+
+import autoBind from 'react-autobind'
+import cx from 'classnames'
+import $ from 'cash-dom'
+
+
+class ColorSchemeToggle extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      dark: false,
+    }
+    autoBind(this)
+  }
+
+  toggleScheme () {
+    if (this.state.dark) {
+      $('body').removeClass('pt-dark')
+    } else {
+      $('body').addClass('pt-dark')
+    }
+    this.setState({ dark: !this.state.dark })
+  }
+
+  render () {
+    const classnames = cx(
+      'pt-minimal',
+      { 'pt-intent-warning': this.state.dark }
+    )
+
+    return (
+      <Button
+        className={classnames}
+        onClick={this.toggleScheme}
+        iconName={this.state.dark ? 'flash' : 'moon'}
+      />
+    )
+  }
+}
+
+
+export { ColorSchemeToggle }
