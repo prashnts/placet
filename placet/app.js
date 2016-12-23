30c135534468e77f2e06b24418bacc018f9ff8f5
Prashant Sinha <prashant+git@noop.pw>
2016-12-24 01:16:39 +0530

    Updated app to better support newer component renders

--- placet/app.js
+++ placet/app.js
@@ -2,11 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import Placet from './layout'
+import { ColorSchemeToggle } from './utils'
 
+
+function _renderComponent(id, component) {
+  ReactDOM.render(component, document.getElementById(id))
+}
 
 function init () {
-  const container = document.getElementById('placetRoot')
-  ReactDOM.render(<Placet />, container)
+  _renderComponent('placetRoot', <Placet/>)
+  _renderComponent('colorSchemeToggle', <ColorSchemeToggle/>)
 }
 
 export { init }
