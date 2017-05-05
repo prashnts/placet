cadb85d7e5cf08254680e6855b52b6d87979e155
Prashant Sinha <prashant+git@noop.pw>
2017-05-05 21:09:17 +0530

    Implement TissueSelector component

--- placet/components/tissue-selector.jsx
+++ placet/components/tissue-selector.jsx
@@ -1,41 +1,39 @@
 import React, { Component } from 'react'
-import { Slider } from '@blueprintjs/core'
 
 import autoBind from 'react-autobind'
 
 const TISSUES = [
-  'A1C',
-  'CBC',
-  'HIP',
-  'ITC',
-  'MD',
-  'OFC',
-  'STC',
-  'V1C',
-  'AMY',
-  'DFC',
-  'IPC',
-  'M1C',
-  'MFC',
-  'S1C',
-  'STR',
-  'VFC',
+  { id: 'A1C', label: 'Primary auditory cortex' },
+  { id: 'AMY', label: 'Amygdala' },
+  { id: 'CBC', label: 'Cerebellar cortex' },
+  { id: 'DFC', label: 'Dorsolateral prefrontal cortex' },
+  { id: 'HIP', label: 'Hippocampus' },
+  { id: 'IPC', label: 'Posterior inferior parietal cortex' },
+  { id: 'ITC', label: 'Inferior temporal cortex' },
+  { id: 'M1C', label: 'Primary motor cortex' },
+  { id: 'MD:', label: 'Mediodorsal nucleus of thalamus' },
+  { id: 'MFC', label: 'Medial prefrontal cortex' },
+  { id: 'OFC', label: 'Orbital prefrontal cortex' },
+  { id: 'S1C', label: 'Primary somatosensory cortex' },
+  { id: 'STC', label: 'Superior temporal cortex' },
+  { id: 'STR', label: 'Striatum' },
+  { id: 'V1C', label: 'Primary visual cortex' },
+  { id: 'VFC', label: 'Ventrolateral prefrontal cortex' },
 ]
 
 
 class TissueSelector extends Component {
   constructor (props) {
     super(props)
-    this.state = {
-      value: props.value,
-    }
+    this.state = { ...props }
     this.emitChange = props.onChange
     autoBind(this)
   }
 
-  changeStage (value) {
-    this.setState({ value: value })
-    this.emitChange(value)
+  changeStage (event) {
+    const val = event.target.value
+    this.setState({ value: val })
+    this.emitChange(val)
   }
 
   componentWillReceiveProps (props) {
@@ -44,7 +42,14 @@ class TissueSelector extends Component {
 
   render () {
     return (
-      <div/>
+      <div className='pt-select'>
+        <select onChange={this.changeStage}
+                value={this.state.value}>
+          {TISSUES.map(t =>
+            <option value={t.id} key={t.id}>{t.label}</option>
+          )}
+        </select>
+      </div>
     )
   }
 }
