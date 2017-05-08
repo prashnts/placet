aaf1b19dc133e8e6c8826fc8481fcd184974e21d
Prashant Sinha <prashant+git@noop.pw>
2017-05-09 00:32:48 +0530

    Update test import

--- test/graph_test.js
+++ test/graph_test.js
@@ -1,6 +1,6 @@
 import { expect } from 'chai'
 
-import Graph, { SigmaGraph } from '~/placet/components/graph'
+import Graph, { SigmaGraph } from '~/placet/models/graph'
 
 const graph_stub = {
   nodes: [
@@ -30,6 +30,17 @@ describe('SigmaGraph', function () {
       g.updateNodeAttr('n0', 'answer', 42)
       expect(g.nodes('n0')).to.have.property('answer').that.equals(42)
     })
+
+    it('Updates computed attributes when node is updated', function () {
+      g.addComputedNodeAttr('r', n => g.degree(n.id) * (n.e || 0))
+      g.initComputedNodeAttr()
+
+      expect(g.nodes().map(n => n.r)).to.deep.equal([0, 0, 0, 0])
+
+      g.updateNodeAttr('n2', 'e', 2)
+
+      expect(g.nodes('n2').r).to.equal(2 * 2)
+    })
   })
 
   describe('#getNodeIds', function () {
@@ -37,27 +48,64 @@ describe('SigmaGraph', function () {
       expect(g.getNodeIds()).to.deep.equal(['n0', 'n1', 'n2', 'n3'])
     })
   })
+
+  describe('#addComputedNodeAttr', function () {
+    it('Can add computed attributes', function () {
+      g.addComputedNodeAttr('label', node => node.id )
+      g.addComputedNodeAttr('foo', () => 'bar' )
+      g._recomputeNodeAttrs('n0')
+      expect(g.nodes('n0')).to.have.property('label').that.equals('n0')
+      expect(g.nodes('n0')).to.have.property('foo').that.equals('bar')
+    })
+  })
+
+  describe('#initComputedNodeAttr', function () {
+    it('Initializes all computed attributes', function () {
+      g.addComputedNodeAttr('label', node => `yo-${node.id}` )
+      g.initComputedNodeAttr()
+      expect(g.nodes().map(n => n.label))
+        .to.deep.equal(['yo-n0', 'yo-n1', 'yo-n2', 'yo-n3'])
+    })
+  })
 })
 
 
 describe('Graph Model', function () {
+  describe('#function aliases', function () {
+    const g = new Graph({ graph: graph_stub })
+
+    it('Has correct aliases', function () {
+      expect(g).to.respondTo('node')
+      expect(g).to.respondTo('edge')
+      expect(g).to.respondTo('degree')
+      expect(g).to.respondTo('read')
+    })
+
+    it('Proxies #nodes via #node', function () {
+      expect(g.node('n0')).to.deep.equal({ id: 'n0' })
+    })
+
+    it('Proxies #edges via #edge', function () {
+      expect(g.edge('e0')).to.deep.equal({ id: 'e0', source: 'n0', target: 'n1' })
+    })
+
+    it('Proxies #degree', function () {
+      expect(g.degree('n0')).to.equal(2)
+    })
+
+    it('Proxies #read', function () {
+      const gr2 = new Graph()
+      expect(gr2.nodes).to.deep.equal([])
+      expect(gr2.edges).to.deep.equal([])
+      gr2.read(graph_stub)
+      expect(gr2.nodes).to.deep.equal(graph_stub.nodes)
+      expect(gr2.edges).to.deep.equal(graph_stub.edges)
+    })
+  })
+
   it('Can initialize with a graph', function () {
     const g = new Graph({ graph: graph_stub })
     expect(g.nodes).to.deep.equal(graph_stub.nodes)
     expect(g.edges).to.deep.equal(graph_stub.edges)
-  })
-
-  it('Allows reading graph', function () {
-    const g = new Graph()
-    expect(g.nodes).to.deep.equal([])
-    expect(g.edges).to.deep.equal([])
-    g.read(graph_stub)
-    expect(g.nodes).to.deep.equal(graph_stub.nodes)
-    expect(g.edges).to.deep.equal(graph_stub.edges)
-  })
-
-  it('Has degree helpers', function () {
-    const g = new Graph({ graph: graph_stub })
-    expect(g.degree('n0')).to.equal(2)
   })
 })
