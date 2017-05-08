b80634d59a8e15ab9546bee1bb064b636be419ca
Prashant Sinha <prashant+git@noop.pw>
2017-05-09 00:33:08 +0530

    Add expression level graph model

---
+++ placet/models/expression-graph.js
@@ -0,0 +1,31 @@
+import * as d3 from 'd3'
+
+import Graph from './graph'
+
+const coolScheme = d3.interpolateCool
+const GREY = d3.color('#ccc')
+
+const radiiScale = d3.scalePow()
+  .domain([0, 1])
+  .range([5, 40])
+
+
+class ExpressionGraph extends Graph {
+  constructor (props) {
+    super(props)
+    this._g.addComputedNodeAttr('r', n => n.expr ? radiiScale(n.expr) : 1)
+    this._g.addComputedNodeAttr('fill', n => n.expr ? coolScheme(n.expr) : GREY)
+    this._g.addComputedNodeAttr('stroke', n => {
+      return n.expr ? d3.color(coolScheme(n.expr)).darker(1) : GREY.darker(1)
+    })
+    this._g.initComputedNodeAttr()
+  }
+
+  updateWithExpressions (expr) {
+    this._g.getNodeIds().forEach(id => {
+      this._g.updateNodeAttr(id, 'expr', expr[id] || 0)
+    })
+  }
+}
+
+export default ExpressionGraph
